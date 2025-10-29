d35a56ef839b18a6a88ce7bd9ddd86bbb502c2b8
Facu Ramirez <ramirezfacundoo01@gmail.com>
2025-10-29 11:40:23 -0300

    Archivos y bases de las funciones principales agregadas

--- funciones_menu.py
+++ funciones_menu.py
@@ -1,12 +1,30 @@
 # ==================================
 #   FUNCIONES PRINCIPALES DEL MENÚ
 # ==================================
+import csv
 
 def buscar_paises():
-    print("")
+    pais_b = input("Ingrese el pais a buscar: ")
+
+    try:
+        print("")
+    except Exception as e:
+        print(f'❌¡Error inesperado! {e}')
 
 def filtrar_paises():
-    print("")
+    print("\n-----------------------")
+    print("    Filtrar países por:  ")
+    print("1. Continente")
+    print("2. Rango de población")
+    print("3. Rango de superficie")
+    filtrado = int(input("Ingrese una opción(número): "))
+
+    try:
+        print("")
+    except ValueError:
+        print("❌¡Error! Debe ingresar un número.")
+    except Exception as e:
+        print(f'❌¡Error inesperado! {e}')
 
 def ordenar_paises():
     print("")
